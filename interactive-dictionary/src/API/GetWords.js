ec2e080cc582cb69e87a8bcb281c94e8b20ec3cf
Aryan2727-debug <78311368+Aryan2727-debug@users.noreply.github.com>
2024-04-20 14:42:25 +0530

    added API fetching logic in GetWords component

--- interactive-dictionary/src/API/GetWords.js
+++ interactive-dictionary/src/API/GetWords.js
@@ -1,1 +1,15 @@
 // this module fetches word definitions and audio pronounciations from an external API
+
+export async function fetch_words(word) {
+    const dictionaryApi = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    try {
+        // fetching data from the API
+        const response = await fetch(dictionaryApi);
+        // retrieving the response in JSON format
+        const data = await response.json();
+        // returning the response
+        return data;
+    } catch (error) {
+        console.log(`Error fetching the dictionary API - ${error}`);
+    }
+};
